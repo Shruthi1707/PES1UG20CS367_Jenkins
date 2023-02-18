a2e8cb783d20fd84b3c796e0b37a038061248a61
Shruthi1707 <77874220+Shruthi1707@users.noreply.github.com>
2023-02-18 15:26:45 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout<<"PES1UG20CS367\n"
+  cout<<"SRN:PES1UG20CS367\n";
+  cout<<"Name: Sai Shruthi\n";
   return 0;
 }
